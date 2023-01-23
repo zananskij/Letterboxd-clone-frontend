920da2cb85561753e57a0a77cc73e2bd86beed06
zananskij <zananskij@gmail.com>
2023-01-22 21:52:09 -0500

    resolve

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,17 @@ import { UserContext } from "../context"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 
+// interface Props {
+//   handleLogin: (user: { username: string; password: string }) => void
+// }
+// interface State {
+//   user: {
+//     username: string
+//     password: string
+//   }
+//   error: string | null
+// }
+
 interface User {
   id: number | null
   username: string
